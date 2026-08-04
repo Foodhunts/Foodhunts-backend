1ccceb0bdf1b940b847623d76deb0aeaae1852a5
henrycodes-ops <henryfalolu1@gmail.com>
2026-08-04 11:15:29 +0100

    feat: add kyc_status field to RestaurantResource

--- app/Http/Resources/RestaurantResource.php
+++ app/Http/Resources/RestaurantResource.php
@@ -28,6 +28,7 @@ class RestaurantResource extends JsonResource
             'type' => $this->type,
             'business_hours' => $this->business_hours,
             'is_active' => $this->is_active,
+            'kyc_status' => $this->kyc_status,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
